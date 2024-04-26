fd274660c2ef1ea6aac71a5ef6a4c29dd1e78297
oyajun <oyamadajun@outlook.jp>
2024-04-26 23:03:25 +0900

    fix: 回答のuseStateに型を明示  BlockMath に変更

--- src/app/solve/page.tsx
+++ src/app/solve/page.tsx
@@ -1,36 +1,38 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Box, Link, TextField, Typography } from "@mui/material";
-import { InlineMath } from "react-katex";
+import { Box, Button, Link, TextField, Typography } from "@mui/material";
+import { InlineMath, BlockMath } from "react-katex";
 import 'katex/dist/katex.min.css';
 import Stack from '@mui/material/Stack';
 
 export default function Solve() {
 
-    let [answer, setAnswer] = useState();
+    let [answer, setAnswer] = useState<string>();
 
     function AnswerBox() {
         if (answer) {
             return (
-                <InlineMath math={answer} />
+                <BlockMath math={answer} />
             )
         } else {
             return (
-                <InlineMath>?</InlineMath>
-
+                <BlockMath math={"?"} />
             )
         }
     }
 
     return (
         <main>
-            <Stack spacing={2}>
-                <Box>
-                    <InlineMath>\int_0^\infty x^2 dx</InlineMath>
+            <Stack className="katex_output" spacing={2}>
+
+                <Box
+                    display="flex"
+                >
+                    <BlockMath> \int_0^\infty x^2 dx</BlockMath>
                 </Box>
                 <Box
                     display="flex"
-                    minHeight={40}
+                    minHeight={70}
                     alignItems="center"
                 >
                     <AnswerBox />
@@ -43,6 +45,7 @@ export default function Solve() {
                         size="small"
                         variant="filled"
                         multiline
+                        value={answer}
                         inputProps={{ style: { fontFamily: "monospace" }, className: "language-typescript" }}
                         onChange={(e) => { setAnswer(e.target.value) }}
                     />
