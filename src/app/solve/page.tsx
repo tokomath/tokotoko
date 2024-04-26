550c71a8f5f4de1912821a72e85a213fa4733542
oyajun <oyamadajun@outlook.jp>
2024-04-26 22:29:34 +0900

    fix: 何も入力されていないときは? 解答欄を等幅で表示

--- src/app/solve/page.tsx
+++ src/app/solve/page.tsx
@@ -1,31 +1,56 @@
 "use client";
-import { TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, Link, TextField, Typography } from "@mui/material";
 import { InlineMath } from "react-katex";
 import 'katex/dist/katex.min.css';
-import { useState } from "react";
+import Stack from '@mui/material/Stack';
 
 export default function Solve() {
-    let [answer, setAnswer] = useState("\\frac{1}{2}");
+
+    let [answer, setAnswer] = useState();
+
+    function AnswerBox() {
+        if (answer) {
+            return (
+                <InlineMath math={answer} />
+            )
+        } else {
+            return (
+                <InlineMath>?</InlineMath>
+
+            )
+        }
+    }
 
     return (
         <main>
-            <div>
-                <InlineMath>\int_0^\infty x^2 dx</InlineMath>
-            </div>
-
-            <div>
-                <TextField
-                    hiddenLabel
-                    id="filled-hidden-label-normal"
-                    fullWidth
-                    onChange={(e) => { setAnswer(e.target.value) }}
-                />
-            </div>
-
-            <div>
-                <InlineMath>{answer}</InlineMath>
-            </div>
+            <Stack spacing={2}>
+                <Box>
+                    <InlineMath>\int_0^\infty x^2 dx</InlineMath>
+                </Box>
+                <Box
+                    display="flex"
+                    minHeight={40}
+                    alignItems="center"
+                >
+                    <AnswerBox />
+                </Box>
+                <Box>
+                    <TextField
+                        hiddenLabel
+                        helperText="回答をKaTeXで入力"
+                        fullWidth
+                        size="small"
+                        variant="filled"
+                        multiline
+                        inputProps={{ style: { fontFamily: "monospace" }, className: "language-typescript" }}
+                        onChange={(e) => { setAnswer(e.target.value) }}
+                    />
+                </Box >
+                <Link href="https://katex.org/docs/supported.html" target="_blank" rel="noopener">
+                    KaTeXチートシート
+                </Link>
+            </Stack>
         </main >
-
     )
 }
