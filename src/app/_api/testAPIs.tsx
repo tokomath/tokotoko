0886ac12947a863a465a6a6fcadad9ecbc5f907c
pusin819 <shibata0819haru@gmail.com>
2024-04-27 14:52:51 +0900

    fix: remake schema.prisma and remake json

---
+++ src/app/_api/testAPIs.tsx
@@ -0,0 +1,54 @@
+"use server"
+
+import {Question, Test, Section, SubSection, SubSubSection} from "@prisma/client";
+
+export interface TestFrame {
+    test: Test,
+    questions: Question[],
+    sections: SectionFrame[],
+}
+
+export interface SectionFrame {
+    section: Section,
+    subSections: SubSectionFrame[],
+    questions: Question[],
+}
+
+export interface SubSectionFrame {
+    subSection: SubSection,
+    subSubSections: SubSubSectionFrame[],
+    questions: Question[],
+}
+
+export interface SubSubSectionFrame {
+    subSubSection: SubSubSection,
+    questions: Question[],
+}
+
+export const createTest = async (props: TestFrame) => {
+    const url = '/api/createTest'
+
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(props),
+    };
+
+    await fetch(url, params);
+}
+
+export const removeTest = async (props: Test) => {
+    const url = '/api/removeTest'
+
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(props),
+    };
+
+    await fetch(url, params);
+}
