9e45831223ab1b579a74b0cd100841243feb32a6
oyajun <oyamadajun@outlook.jp>
2024-04-27 09:51:14 +0900

    add: 問題ごとにカードに

--- src/app/solve/page.tsx
+++ src/app/solve/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Box, Button, Link, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, Link, TextField, Typography } from "@mui/material";
 import { InlineMath, BlockMath } from "react-katex";
 import 'katex/dist/katex.min.css';
 import Stack from '@mui/material/Stack';
@@ -23,42 +23,50 @@ export default function Solve() {
 
     return (
         <main>
-            <Stack spacing={2}>
-                <Box
-                    display="flex"
-                >
-                    <BlockMath> \int_0^\infty x^2 dx</BlockMath>
-                </Box>
-                <Box
-                    display="flex"
-                    minHeight={70}
-                    alignItems="center"
-                >
-                    <AnswerBox />
-                </Box>
-                <Box>
-                    <TextField
-                        hiddenLabel
-                        helperText="回答をKaTeXで入力"
-                        fullWidth
-                        size="small"
-                        variant="filled"
-                        multiline
-                        value={answer}
-                        inputProps={{ style: { fontFamily: "monospace" }, className: "language-typescript" }}
-                        onChange={(e) => { setAnswer(e.target.value) }}
-                    />
-                </Box >
-                <Button variant="outlined" onClick={() => setAnswer(answer + "\\int")}>
-                    <InlineMath math="\int"></InlineMath>
-                </Button>
-                <Button variant="outlined" onClick={() => setAnswer(answer + "\\begin{pmatrix}\n  a & b \\\\\n  c & d\n\\end{pmatrix}")}>
-                    <InlineMath math="\begin{pmatrix}a & b \\\\ c & d \end{pmatrix}"></InlineMath>
-                </Button>
-                <Link href="https://katex.org/docs/supported.html" target="_blank" rel="noopener">
-                    KaTeXチートシート
-                </Link>
-            </Stack>
+            <Box
+                maxWidth="md"
+            >
+                <Card variant="outlined">
+                    <CardContent>
+                        <Stack spacing={1}>
+                            <Box
+                                display="flex"
+                            >
+                                <BlockMath> \int_0^\infty x^2 dx</BlockMath>
+                            </Box>
+                            <Box
+                                display="flex"
+                                minHeight={80}
+                                alignItems="center"
+                            >
+                                <AnswerBox />
+                            </Box>
+                            <Box>
+                                <TextField
+                                    hiddenLabel
+                                    helperText="回答をKaTeXで入力 改行可能"
+                                    fullWidth
+                                    size="small"
+                                    variant="filled"
+                                    multiline
+                                    value={answer}
+                                    inputProps={{ style: { fontFamily: "monospace" } }}
+                                    onChange={(e) => { setAnswer(e.target.value) }}
+                                />
+                            </Box >
+                            <Button variant="outlined" onClick={() => setAnswer(answer + "\\int")}>
+                                <InlineMath math="\int"></InlineMath>
+                            </Button>
+                            <Button variant="outlined" onClick={() => setAnswer(answer + "\\begin{pmatrix}\n  a & b \\\\\n  c & d\n\\end{pmatrix}")}>
+                                <InlineMath math="\begin{pmatrix}a & b \\\\ c & d \end{pmatrix}"></InlineMath>
+                            </Button>
+                            <Link href="https://katex.org/docs/supported.html" target="_blank" rel="noopener">
+                                KaTeXチートシート
+                            </Link>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Box>
         </main >
     )
 }
