8e954b45450938ab45365e9bb02d4a461fc6db9a
oyajun <oyamadajun@outlook.jp>
2024-04-29 04:33:44 +0900

    フッターを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import "./globals.css";
+import { Box, Stack, Typography } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,16 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        <footer>
+          <Box maxWidth={640}>
+
+          </Box>
+          <Stack
+            spacing={2} maxWidth={640} paddingTop={2} paddingBottom={2} margin="auto" alignItems={"center"}
+          >
+            <Typography fontSize={28} >TokoToko / とことこ</Typography>
+          </Stack>
+        </footer>
       </body>
     </html>
   );
