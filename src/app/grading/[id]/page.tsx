5f8fec933edf2008e3eaf4c185ab6f46ecc2ffa2
RunaChama <rui1412jp@gmail.com>
2024-07-09 17:12:00 +0900

    [update]idをもとにAPIから問題を取得

--- src/app/grading/[id]/page.tsx
+++ src/app/grading/[id]/page.tsx
@@ -1,113 +1,71 @@
 "use client";
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { Paper,Typography } from "@mui/material";
 import { Table,TableContainer, TableBody, TableHead, TableRow,TableCell } from "@mui/material";
 import { InlineMath } from "react-katex";
 import Latex from "react-latex-next";
 import 'katex/dist/katex.min.css';
+import axios, { AxiosResponse } from "axios";
+import { error } from "console";
 
-interface Answer{
-    id:string;
-    number:string;
-    answer:string;
-}
-
-interface StudentAnswer{
-    name:string;
-    answers:Answer[];
-}
-
-function createAnsData(id:string,number:string,answer:string)
-: Answer{
-    return {id,number,answer};
-}
-
-function createStudentAnsData(name:string, answers:Answer[])
-: StudentAnswer{
-    return {name,answers};
-}
-
-export default function Grading({ params }: { params: { id: string } })
+//#region interface宣言
+interface Question
 {
-    let assignment = {
-        title: "課題1",
-        description: "積分の問題です",
-    };
+    id: number;
+    subSectionId: number;
+    number: number;
+    question: string;
+    answer: string;
+}
 
-    let questions = [
-        {
-            id: "0",
-            number: "Q1",
-            question: "$ \\int x^3 dx $ を解け",
-          },
-          {
-            id: "1",
-            number: "Q2",
-            question: "$ \\int x^4 dx $ を解け",
-          },
-          {
-            id: "2",
-            number: "Q3",
-            question: "$ \\int x^5 dx $ を解け",
-          }
-    ];
+interface SubSection
+{
+    id: number;
+    number: number;
+    sectionId: number;
+    summary: string;
+    questions: Question[];
+}
 
-    let student_answers = [
-        createAnsData("0","Q1","$ \\frac{1}{4} x^4$"),
-        createAnsData("1","Q2","$ \\frac{1}{5} x^5$"),
-        createAnsData("2","Q3","$ \\frac{1}{6} x^6$"),
-    ]
-    
-    let rows = [];
-    for(let i = 1;i < 41;i++)
+interface Section
+{
+    id: number;
+    number: number;
+    summary: string;
+    subSections: SubSection[];
+}
+
+interface TestData
+{
+    id : number;
+    title: string;
+    summary: string;
+    startDate: Date;
+    endDate: Date;
+    sections: Section[];
+}
+
+//#endregion
+
+
+export default function Grading({ params }: { params: { id: number } })
+{
+    const [test_data,set_test_data] = useState<TestData|null>(null);
+
+    useEffect(()=>
     {
-        let data = createStudentAnsData("Sample Guy"+i,student_answers);
-        rows.push(data);
-    }
+        axios.post("/api/test/get",{ id: Number(params.id)})
+        .then(response =>{
+            set_test_data(response.data);
+        });
+    },[])
 
+    
+    //console.log(test);
     return (
         <>
-        <Paper >
-            <Table stickyHeader>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            <Typography>問題ID:{params.id}</Typography>
-                            <Typography>出席番号</Typography>
-                        </TableCell>
-                        {
-                            questions.map((question, index) => {
-                                return (
-                                    <TableCell key={"index-header0-${question.id}"}>
-                                        {question.number}<br />
-                                        <Latex>{question.question}</Latex>
-                                    </TableCell>
-                                    
-                                );
-                            })
-                        }
-                    </TableRow>
-                </TableHead>
+        <Paper>
 
-                <TableBody>
-                    {
-                        rows.map((row) => {
-                            return (
-                                <TableRow key="-$rowname-{row.name}">
-                                    <TableCell>{row.name}</TableCell>
-                                    {
-                                        row.answers.map((answer) => {
-                                            return (
-                                                <TableCell key={answer.id}><Latex>{answer.answer}</Latex></TableCell>
-                                            );
-                                        })
-                                    }
-                                </TableRow>
-                                );
-                        })
-                    }
-                </TableBody>
-            </Table>
         </Paper>
         </>
     )
