a2c621d358e919320abc11cb05c8fda7c572f01b
pusin819 <shibata0819haru@gmail.com>
2024-07-13 11:40:36 +0900

    fix: delete unused function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-"use client"
-import { Button } from '@mui/material';
+"use client";
+import { Button } from "@mui/material";
 import { Stack } from "@mui/system";
 import { useCallback, useState } from "react";
 import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
-import { removeTest, createTest, DeleteTestProps, getTest } from "@/app/api/testAPIs";
+import {
+  removeTest,
+  createTest,
+  DeleteTestProps,
+  getTest,
+} from "@/app/api/testAPIs";
 import { Question, Test } from "@prisma/client";
 import { TestFrame, SectionFrame, SubSectionFrame } from "@/app/api/testAPIs";
 import dayjs from "dayjs";
 
 export default function Home() {
-  return (
-    <FormControl>
-
-    </FormControl>
-  )
   /*
   const [num, setNum] = useState(0);
   const send = async () => {
