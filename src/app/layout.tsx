7bd18379978e5e3b10c0070bb24a93e2473b9dfb
oyajun <oyamadajun@outlook.jp>
2024-07-20 09:00:09 +0900

    update: ソースコードとサードパーティーのリンクをフッターに追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,35 @@
 "use client"
-import {Inter} from "next/font/google";
-import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter";
+import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import "./globals.css";
-import {Typography} from "@mui/material";
-import {SessionProvider} from "next-auth/react";
+import { Link, Typography } from "@mui/material";
+import { SessionProvider } from "next-auth/react";
+import { Box } from "@mui/system";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-    <SessionProvider>
-      <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-    </SessionProvider>
-    <footer>
-      <Typography fontSize={14} textAlign={"center"} margin={2}>TokoToko / とことこ</Typography>
-    </footer>
-    </body>
-    </html>
+      <body className={inter.className}>
+        <SessionProvider>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </SessionProvider>
+        <footer>
+          {/* 中央寄せ */}
+          <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+            <Typography fontSize={17}>TokoToko / とことこ</Typography>
+            <Box display="flex" justifyContent="center" alignItems="center" flexDirection="row" marginTop={1}>
+              <Link href="https://github.com/tokomath/tokotoko" marginRight={1}>Source Code</Link>
+              <Link href="https://github.com/tokomath/tokotoko/blob/main/ThirdPartyNotices.txt" marginLeft={1}>Third Party Licenses</Link>
+            </Box>
+          </Box>
+        </footer>
+      </body>
+    </html >
   );
 }
