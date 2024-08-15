ebc9efe0dd280bda69ef96e5eda8ba45ec826fa6
pusin819 <shibata0819haru@gmail.com>
2024-08-15 11:33:08 +0900

    add useSession

--- src/app/teacher/manageClass/page.tsx
+++ src/app/teacher/manageClass/page.tsx
@@ -12,81 +12,81 @@ import {
   Tooltip,
   Typography
 } from "@mui/material";
-import {useEffect, useState} from "react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import ClassIcon from '@mui/icons-material/Class';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
-import {Class} from "@prisma/client";
-import {NumberInput} from "@mui/base/Unstable_NumberInput/NumberInput";
+import { Class } from "@prisma/client";
+import { NumberInput } from "@mui/base/Unstable_NumberInput/NumberInput";
 import AddIcon from "@mui/icons-material/Add";
 
-import {getAllClass, getClassByUser} from "@/app/api/class/getClass";
+import { getAllClass, getClassByUser } from "@/app/api/class/getClass";
 
 export default function Page() {
-  const YourClassList = () => {
-    const [teacherName, setTeacherName] = useState("")
-    const [classes, setClasses] = useState<Class[]>([])
-
-    // TODO: get current teacherId
-    useEffect(() => {
-      const fetchClass = async () => {
-        const tmpClassList = await getClassByUser(teacherName)
-        // const tmpClassList = await getAllClass()
-        setClasses(tmpClassList)
-        console.log(tmpClassList)
-      }
-
-      fetchClass()
-    }, [teacherName])
-
-    console.log(classes)
-    return (
-      <Stack alignItems={"center"}>
-        <Typography>
-          Your Classes
-        </Typography>
-        <TextField
-          value={teacherName}
-          onChange={(e: any) => {
-            setTeacherName(e.target.value)
-          }}
-        />
-        {classes.map((c: Class, i: number) => {
-          return (
-            <ListItem key={i}>
-              <ListItemIcon>
-                <ClassIcon/>
-              </ListItemIcon>
-              <ListItemText
-                primary={c.name}
-              />
-              <Tooltip title={"add student"}>
-                <IconButton>
-                  <PersonAddAlt1Icon/>
-                </IconButton>
-              </Tooltip>
-            </ListItem>
-          )
-        })}
-
-        <ListItem>
-          <Box width={"100%"} display={"flex"} justifyContent={"center"}>
-            <Tooltip title={"create new class"}>
-              <IconButton>
-                <AddIcon/>
-              </IconButton>
-            </Tooltip>
-          </Box>
-        </ListItem>
-      </Stack>
-    )
-  }
-
   return (
     <Stack>
       <Typography>
         create class
       </Typography>
-      <YourClassList/>
+      <YourClassList />
+    </Stack>
+  )
+}
+
+const YourClassList = () => {
+  const { data: session, status } = useSession()
+
+  const [teacherName, setTeacherName] = useState<string>(session?.user.name || "")
+  const [classes, setClasses] = useState<Class[]>([])
+
+  useEffect(() => {
+    const fetchClass = async () => {
+      const tmpClassList = await getClassByUser(teacherName)
+      // const tmpClassList = await getAllClass()
+      setClasses(tmpClassList)
+      console.log(tmpClassList)
+    }
+
+    fetchClass()
+  }, [teacherName])
+
+  console.log(classes)
+  const moveToCreateClass = () => {
+    location.href = "createClass"
+  }
+
+  return (
+    <Stack alignItems={"center"}>
+      <Typography>
+        {teacherName}&#39;s classes
+      </Typography>
+      {classes.map((c: Class, i: number) => {
+        return (
+          <ListItem key={i}>
+            <ListItemIcon>
+              <ClassIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={c.name}
+            />
+            <Tooltip title={"add student"}>
+              <IconButton >
+                <PersonAddAlt1Icon />
+              </IconButton>
+            </Tooltip>
+          </ListItem>
+        )
+      })}
+
+      <ListItem>
+        <Box width={"100%"} display={"flex"} justifyContent={"center"}>
+          <Tooltip title={"create new class"}>
+            <IconButton onClick={moveToCreateClass}>
+              <AddIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </ListItem>
     </Stack>
   )
 }
