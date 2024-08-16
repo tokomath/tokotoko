83216aeab0cd023b92716de72fa267560aa056d1
RunaChama <rui1412jp@gmail.com>
2024-08-16 09:44:37 +0900

    [feat]getClassMemberById

--- src/app/api/class/getClassMember.ts
+++ src/app/api/class/getClassMember.ts
@@ -2,6 +2,21 @@
 
 import {prisma} from "@/app/api/prisma_client"
 
-export const getClassMemberById = async() => {
-    
+export const getClassMemberById = async(classID : Number) => {
+    const response = await prisma.class.findUnique({
+        where: {id: classID},
+        include: {
+            users: {
+                include : {
+                    id : true,
+                    name : true
+                }
+            }
+        }
+    });
+
+    if(response)
+        return response;
+    else
+        return null;
 }
