744ca4c0087ec2a9ede7ab177427634aa7c1c8f1
yuma <chroma31415@gmail.com>
2024-08-21 21:21:22 +0900

    fix: delete Debug message

--- src/app/teacher/layout.tsx
+++ src/app/teacher/layout.tsx
@@ -3,9 +3,6 @@ import {TeacherGuard} from "@/lib/guards/userGuard";
 
 export default async function TeacherLayout({children}: { children: React.ReactNode }) {
   return (
-    <>
-      <h1>Debug: TeacherLayout</h1>
-      <TeacherGuard>{children}</TeacherGuard>
-    </>
+    <TeacherGuard>{children}</TeacherGuard>
   )
 }
