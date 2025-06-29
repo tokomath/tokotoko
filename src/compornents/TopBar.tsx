c3f8e2de94eb0632699a930d590fdcc861cc9ba1
RunaChama <rui1412jp@gmail.com>
2025-06-29 18:26:37 +0900

    fix/username text on topbar

--- src/compornents/TopBar.tsx
+++ src/compornents/TopBar.tsx
@@ -16,26 +16,31 @@ interface QuestionProps {
 }
 
 export default function TopBar({ page_name = "" }: QuestionProps) {
-  const { user } = useUser();
+  const { user, isSignedIn } = useUser();
   const router = useRouter();
   const titleEvent = () => {
     router.push("/");
   }
+  let name: string = "";
+  if (isSignedIn)
+    name = user?.firstName + " " + user?.lastName;
   return (
     <>
       <Toolbar style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
         <Typography variant="h6" component="div" width="calc(100%/3)" textAlign="left">
-          <Button variant="text" onClick={titleEvent} sx={{color:"white",fontSize:"22px"}}>Formula Form</Button>
+          <Button variant="text" onClick={titleEvent} sx={{ color: "white", fontSize: "22px" }}>Formula Form</Button>
         </Typography>
         <Box width="calc(100%/3)" textAlign="center">
           <Typography variant="h4">{page_name}</Typography>
         </Box>
-        <Box width="calc(100%/3)" textAlign="right">
-          <Box display="flex" justifyContent="flex-end">
-            <Typography>{user?.firstName + " " + user?.lastName}</Typography>
+        <Box width="calc(100%/3)" textAlign="right" >
+          <Box display="flex" justifyContent="flex-end" alignItems="center">
+            <Typography marginRight="5px" alignItems="center" sx={{ display: "flex", alignItems: "center" }}>
+              {name}
+            </Typography>
             <SignedOut>
               <div className="h-fit ml-4 bg-blue-500 w-fit rounded-md text-white text-sm font-semibold">
-                <SignInButton />
+                <UserButton />
               </div>
             </SignedOut>
             <SignedIn>
