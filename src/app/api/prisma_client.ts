eee30aaf401341ac02815cbe9f4768d5546ca0cc
Rui Kimura <s22114@numazu.kosen-ac.jp>
2026-05-06 21:18:45 +0900

    fix/prisma7対応

--- src/app/api/prisma_client.ts
+++ src/app/api/prisma_client.ts
@@ -1,3 +1,21 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import path from 'path'
 
-export const prisma = new PrismaClient()
+const envUrl = process.env.DATABASE_URL || '';
+
+const relativePath = envUrl.replace('file:', '');
+
+
+const absolutePath = path.resolve(process.cwd(), 'prisma', relativePath);
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  (() => {
+    const adapter = new PrismaBetterSqlite3({ url: absolutePath });
+    return new PrismaClient({ adapter });
+  })();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
